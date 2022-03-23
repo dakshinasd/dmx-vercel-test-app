0d3f778ed95afac22e9a3730224fc20d0d2d07bb
dakshinasd <me@dakshinasd.com>
2022-03-23 10:48:13 +0530

    Changed grid size

---
+++ src/pages/products/[id].js
@@ -0,0 +1,9 @@
+import ProductCard from '../componetns/Product';
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+};
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -21,12 +21,14 @@ const Products = () => {
   }
 
   return (
-    <div className="grid grid-cols-4 gap-5 p-5">
-      {products.length ? (
-        products.map((product) => <Product product={product} />)
-      ) : (
-        <div>No Products Available</div>
-      )}
+    <div className="container">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 p-5">
+        {products.length ? (
+          products.map((product) => <Product product={product} />)
+        ) : (
+          <div>No Products Available</div>
+        )}
+      </div>
     </div>
   );
 };
