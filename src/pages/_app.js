eb95183b2d6cbb1819b90a6a9706476248cc3c1f
dakshinasd <me@dakshinasd.com>
2022-03-24 06:48:27 +0530

    Complete theme configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,47 @@
+import { useState, useEffect } from 'react';
+
 import Header from '../components/Header';
 import { baseTheme, immensaTheme } from '../config/themes.js';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  if (process.browser) {
-    const theme = immensaTheme;
-    const root = document.documentElement;
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const colorObject = theme.configuration.colors;
+  useEffect(() => {
+    if (process.browser) {
+      // simulating fetch of theme configuration from API
+      (async () => {
+        setLoading(true);
+        try {
+          const response = await fetch(
+            'https://my-json-server.typicode.com/dakshinasd/fake-json-server/users'
+          );
+          const fetchedUsers = await response.json();
+          setUsers(fetchedUsers);
+        } catch (err) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      })();
 
-    Object.keys(colorObject).forEach((colorKey) =>
-      root.style.setProperty(colorKey, colorObject[colorKey])
-    );
+      const theme = immensaTheme;
+      const root = document.documentElement;
+
+      const colorObject = theme.configuration.colors;
+
+      Object.keys(colorObject).forEach((colorKey) =>
+        root.style.setProperty(colorKey, colorObject[colorKey])
+      );
+    }
+  }, []);
+
+  if (loading) {
+    return <div className="text-xl p-10 text-center">Loading....</div>;
   }
+
   return (
     <>
       <Header />
